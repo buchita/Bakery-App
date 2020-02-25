1d66f8424181bb367512041b52b67f7303058ca8
Buch <25643076+buchita@users.noreply.github.com>
2020-02-25 17:36:00 +0000

    index page is linked to server

--- server.js
+++ server.js
@@ -2,16 +2,43 @@
 
 
 const http = require('http');
+// filesystem
+let filesystem = require('fs');
+
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const server = http.createServer((req, res) => {
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+var server = http.createServer(function (req, resp) {
+    
+  // reads in the file index.html
+  filesystem.readFile("index.html", function (error, pgResp) {
+      if (error) {
+          resp.writeHead(404);
+          resp.write('Contents you are looking are Not Found');
+      } else {
+          resp.writeHead(200, { 'Content-Type': 'text/html' });
+          resp.write(pgResp);
+      }
+        
+      resp.end();
+  });
+     
 });
+//5.
+server.listen(port);
+ 
+console.log('Server Started listening on ' + port);
